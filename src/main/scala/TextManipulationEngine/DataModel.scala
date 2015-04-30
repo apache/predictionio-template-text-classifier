8d9ac7b86371c86847ffbfa194ba533a7dc1f484
mvivero091 <mvivero091@gmail.com>
2015-04-29 17:18:15 -0700

    one more functional

--- src/main/scala/TextManipulationEngine/DataModel.scala
+++ src/main/scala/TextManipulationEngine/DataModel.scala
@@ -7,86 +7,109 @@ import org.apache.spark.mllib.linalg.Vector
 import org.apache.spark.mllib.linalg.Vectors
 import org.apache.spark.mllib.regression.LabeledPoint
 import org.apache.spark.rdd.RDD
-
 import scala.collection.JavaConversions._
+import scala.collection.immutable.HashMap
 import scala.math.log
+
+
+
 
 
 // This class will take in as parameters an instance of type
 // TrainingData, nMin, nMax which are the lower and upper bounds,
-// respectively, of the model n-gram window. This class serves
-// as an implementation of our data model.
+// respectively, of the model n-gram window.
 
-class DataModel(
-                 td: TrainingData,
-                 nMin: Int,
-                 nMax: Int
-                 ) extends Serializable {
+class DataModel (
+                  td: TrainingData,
+                  nMin: Int,
+                  nMax: Int
+                  ) extends Serializable {
 
 
-  // This private method will tokenize our text entries.
+  // 1. Tokenizer: document => token list.
+  //
+  // Takes an individual document and converts it to
+  // a list of allowable tokens.
 
-  // document => token list
   private def tokenize (doc : String): Array[String] = {
-    SimpleTokenizer.INSTANCE.tokenize(doc)
+    SimpleTokenizer.INSTANCE
+      .tokenize(doc)
+      .filter(e => ! td.stopWords.contains(e))
   }
 
+  // 2. Hasher: Array[tokens] => Map(n-gram -> n-gram document tf).
 
-  // This private method will help convert each text observation
-  // and return a HashMap in which every token that appears in the
-  // document is associated to the number of times it appears in the
-  // document.
+  private def hash (tokenList : Array[String]): Map[String, Double] = {
+    // Initialize an NGramModel from OpenNLP tools library,
+    // and add the list of allowable tokens to the n-gram model.
+    val model : NGramModel = new NGramModel()
+    model.add(new StringList(tokenList: _*), nMin, nMax)
 
-  // Map(token -> token_count)
-  private def hashDoc(doc: String): Map[String, Double] = {
-    val model = new NGramModel()
-    model.add(new StringList(tokenize(doc): _*), nMin, nMax)
-    val map  = model.iterator.map(
-      x => (x.toString, model.getCount(x).toDouble)
-    ).filter(e => ! td.stopWords.contains(e)).toMap
+    val map : Map[String, Double] = model.iterator
+      .map(
+        x => (x.toString, model.getCount(x).toDouble)
+      ).toMap
+
+    // Divide by the total number of n-grams in the document
+    // to obtain n-gram frequency.
     map.mapValues(e => e / map.values.sum)
   }
 
-  private val hashedData = td.data.map(e => hashDoc(e.text)).cache
 
-  // Compute required idf data.
-
-  private val numDocs: Double = hashedData.count.toDouble
-
-  // Creates token-gram universe.
+  // 3. Bigram universe extractor: RDD[bigram hashmaps] => RDD[((n-gram, n-gram idf), global index)]
   private def createUniverse(u: RDD[Map[String, Double]]): RDD[((String, Double), Long)] = {
-    u.flatMap(identity).map(
-      e => (e._1, 1.0)
-    ).reduceByKey(_ + _).map(
-        e => (e._1, log(numDocs / e._2))
-      ).zipWithIndex
+    u.flatMap(identity)
+      .map(e => (e._1, 1.0))
+      .reduceByKey(_ + _)
+      .map(e => (e._1, log(numDocs / e._2)))
+      .zipWithIndex
   }
-  private val universe = createUniverse(hashedData)
 
-  // Map(token -> idf)
-  private val idf : Map[String, Double] = universe.map(_._1).collect.toMap
-  // Map(token -> token index in universe)
-  private val tokenIndex : Map[String, Int] = universe.map(e => (e._1._1, e._2.toInt)).collect.toMap
-  private val numTokens = idf.size
+  // 4. Set private class variables for use in data transformations.
 
+  // Total number of documents (should be 11314).
+  private val numDocs: Double = td.data.count.toDouble
 
-  // Transforms a given string document into a data vector
-  // based on the given data model (tfidf indicates whether
-  // tfidf transformation performed.
+  // Create our n-gram universe.
+  private val universe : RDD[((String, Double), Long)]= createUniverse(
+    td.data
+      .map(e => hash(tokenize(e.text)))
+  ).cache
+
+  // Get total number n-grams in universe (in
+  // bigram case, the number of unique n-grams
+  // is about 1,200,000).
+  private val numTokens : Int = universe.count.toInt
+
+  // Create ngram to idf hashmap:
+  //    Map(n-gram -> n-gram idf)
+  private val idf : HashMap[String, Double] = HashMap(universe.map(_._1).collect : _*)
+
+  // Create n-gram to global index hashmap:
+  //    Map(n-gram -> global index)
+  private val globalIndex : HashMap[String, Int] = HashMap(universe.map(e => (e._1._1, e._2.toInt)).collect : _*)
+
+  // 5. Document Transformer: document => sparse tf-idf vector.
+  // This takes a single document, tokenizes it, hashes it,
+  // and finally returns a sparse vector containing the
+  // tf-idf entries of the document n-grams (0 for all n-grams
+  // not contained in the document).
   def transform(doc: String): Vector = {
-    val hashedDoc = hashDoc(doc)
-    Vectors.sparse(numTokens, hashedDoc.map(e => (tokenIndex.get(e._1).get,
-      e._2 * idf.get(e._1).get)).toArray)
+    // Map(n-gram -> doument tf)
+    val hashedDoc = hash(tokenize(doc))
+    Vectors.sparse(
+      numTokens,
+      hashedDoc.map {
+        case (ngram, tf) => (globalIndex(ngram), idf(ngram) * tf)
+      }.toArray
+    )
   }
 
-
-  // Returns a data instance that is ready to be used for
-  // model training.
+  // 6. Data Transformer: RDD[documents] => RDD[LabeledPoints]
   def transformData: RDD[LabeledPoint] = {
-
-    td.data.map(obs => LabeledPoint(
-        obs.label, transform(obs.text)
-      ))
-    }
+    td.data.map(e => LabeledPoint(e.label, transform(e.text)))
+  }
 
 }
+
+
