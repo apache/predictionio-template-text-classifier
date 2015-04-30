dddaf7bcb46c916b9459430b44501c36cec80ac5
mvivero091 <mvivero091@gmail.com>
2015-04-29 21:55:04 -0700

    cleaned up

--- src/main/scala/TextManipulationEngine/Evaluation.scala
+++ src/main/scala/TextManipulationEngine/Evaluation.scala
@@ -32,20 +32,12 @@ object AccuracyEvaluation extends Evaluation {
 object EngineParamsList extends EngineParamsGenerator {
   // Define list of EngineParams used in Evaluation
 
-  // First, we define the base engine params. It specifies the appId from which
-  // the data is read, and a evalK parameter is used to define the
-  // cross-validation.
+  // Set data source and preparator parameters.
   private[this] val baseEP = EngineParams(
     dataSourceParams = DataSourceParams(appName = "marco-testapp", evalK = Some(5)),
     preparatorParams = PreparatorParams(nMin = 1, nMax = 2))
 
-  // Second, we specify the engine params list by explicitly listing all
-  // algorithm parameters. In this case, we evaluate 3 engine params, each with
-  // a different algorithm params value.
-
-  // In this example we will primarily focus on the appropriate value of the additive smoothing constant,
-  // and leave the number of n-grams fixed. The number of n-grams itself is a model hyperparameter
-  // and should also be tuned.
+  // Set the algorithm params for which we will assess an accuracy score.
   engineParamsList = Seq(
     baseEP.copy(algorithmParamsList = Seq(("sup", SupervisedAlgorithmParams(0.5)))))
 }
--- src/main/scala/TextManipulationEngine/Serving.scala
+++ src/main/scala/TextManipulationEngine/Serving.scala
@@ -2,11 +2,10 @@ package TextManipulationEngine
 
 import io.prediction.controller.LServing
 
+// 1. Define serving component.
 class Serving extends LServing[Query, PredictedResult] {
 
   override
-  def serve(query: Query,
-            predictedResults: Seq[PredictedResult]): PredictedResult = {
-    predictedResults.head
-  }
+  def serve(query: Query, predictedResults: Seq[PredictedResult]):
+  PredictedResult = predictedResults.head
 }
